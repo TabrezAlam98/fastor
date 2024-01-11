7b26f9ef4ff8b0f78a5d5479b2d823527c0ea1b6
Tabrez Alam <97460388+TabrezAlam98@users.noreply.github.com>
2024-01-11 16:59:13 +0530

    created signup and login

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log("hii, node.js")
+const express = require("express");
+require('dotenv').config()
+const { connection } = require("./config/db");
+const Port=3000
+const {usersRoutes}=require('./Routes/users.routes')
+const app = express();
+app.use(express.json());
+
+
+app.use("/users",usersRoutes)
+
+
+app.listen(process.env.Port, async () => {
+  try {
+    await connection;
+    console.log("connected");
+  } catch {
+    console.log(err);
+    console.log("connected mongodb");
+  }
+  console.log(`listening on port ${Port}`);
+});
